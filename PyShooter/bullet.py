cc857c9eae2a2fc665df899f3d739c32d1748116
Davide Gualano <ildave@gmail.com>
2018-02-20 15:56:43 +0100

    Change bullet to a small line

--- PyShooter/bullet.py
+++ PyShooter/bullet.py
@@ -4,29 +4,30 @@ import math
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, ship):
         super().__init__()
-        self.radius = 5
+        self.length = 5
         self.x = int(ship.x + 2 * ship.radius * math.cos(ship.angle))
         self.y = int(ship.y + 2 * ship.radius * math.sin(ship.angle))
         self.angle = ship.angle
-        self.angularspeed = -0.001*math.pi
 
-        self.image = pygame.Surface([self.radius * 2, self.radius * 2])
+        self.image = pygame.Surface([self.length * 2, self.length * 2])
         self.rect = self.image.get_rect()
-        self.rect.x = self.x - self.radius
-        self.rect.y = self.y - self.radius
+        self.rect.x = self.x - self.length
+        self.rect.y = self.y - self.length
         self.color = pygame.color.THECOLORS['cyan']
-        self.hspeed = 0.15
-        self.vspeed = 0.15
+        self.hspeed = 0.2
+        self.vspeed = 0.2
 
     def update(self, elapsed):
-        self.y +=  math.sin(self.angle) * self.vspeed * elapsed 
+        self.y += math.sin(self.angle) * self.vspeed * elapsed 
         self.x += math.cos(self.angle) * self.hspeed * elapsed
 
-        self.rect.x = self.x - self.radius
-        self.rect.y = self.y - self.radius
+        self.rect.x = self.x - self.length
+        self.rect.y = self.y - self.length
         
         if self.x < 0 or self.y < 0 or self.x > 800 or self.y > 600:
             self.kill()
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+        px = int(self.x + 2 * self.length * math.cos(self.angle))
+        py = int(self.y + 2 * self.length * math.sin(self.angle))
+        pygame.draw.line(screen, self.color, (int(self.x), int(self.y)), (int(px), int(py)))
