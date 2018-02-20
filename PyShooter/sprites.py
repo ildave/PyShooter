4f75054d1a8c7fbbc1c9d18430810edd3f159db9
Davide Gualano <ildave@gmail.com>
2018-02-20 11:14:30 +0100

    Fix enemies drawing

--- PyShooter/sprites.py
+++ PyShooter/sprites.py
@@ -99,7 +99,7 @@ class Enemy(pygame.sprite.Sprite):
             self.movement = self.cosMovement
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.rect.x, self.rect.y), self.radius)
+        pygame.draw.circle(screen, self.color, (self.rect.x + self.radius, self.rect.y +  self.radius), self.radius)
 
     def update(self, elapsed, gameScene):
         self.rect.y += self.verticalSpeed * elapsed
@@ -108,10 +108,10 @@ class Enemy(pygame.sprite.Sprite):
             gameScene.missed += 1
         self.rect.x = self.movement()
 
-        if self.rect.x < self.radius:
-            self.rect.x = self.radius
-        if self.rect.x > 800 - self.radius:
-            self.rect.x = 800 - self.radius
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > 800 - self.radius * 2:
+            self.rect.x = 800 - self.radius * 2
 
     def sinMovement(self):
         return -1 * math.sin(self.rect.y / 50) * self.amplitude + self.startX
