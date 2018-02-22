5a53fc4d7297623a6c83db7f3144c69436521866
Davide Gualano <ildave@gmail.com>
2018-02-22 09:49:12 +0100

    Improve bullets data for better collisions handling

--- PyShooter/bullet.py
+++ PyShooter/bullet.py
@@ -6,29 +6,31 @@ class Bullet(pygame.sprite.Sprite):
         super().__init__()
         self.game = game
         self.length = 5
-        self.x = int(ship.x + 2 * ship.radius * math.cos(ship.angle))
-        self.y = int(ship.y + 2 * ship.radius * math.sin(ship.angle))
         self.angle = ship.angle
-
-        self.image = pygame.Surface([self.length * 2, self.length * 2])
+        self.bx = int(ship.x + 2 * ship.radius * math.cos(self.angle))
+        self.by = int(ship.y + 2 * ship.radius * math.sin(self.angle))
+        self.ex = int(self.bx + 2 * self.length * math.cos(self.angle))
+        self.ey = int(self.by + 2 * self.length * math.sin(self.angle))
+        
+        self.image = pygame.Surface([1, 1])
         self.rect = self.image.get_rect()
-        self.rect.x = self.x - self.length
-        self.rect.y = self.y - self.length
+        self.rect.x = self.ex
+        self.rect.y = self.ey
         self.color = pygame.color.THECOLORS['cyan']
         self.hspeed = 0.2
         self.vspeed = 0.2
 
     def update(self, elapsed):
-        self.y += math.sin(self.angle) * self.vspeed * elapsed 
-        self.x += math.cos(self.angle) * self.hspeed * elapsed
+        self.by += math.sin(self.angle) * self.vspeed * elapsed 
+        self.bx += math.cos(self.angle) * self.hspeed * elapsed
+        self.ex = int(self.bx + 2 * self.length * math.cos(self.angle))
+        self.ey = int(self.by + 2 * self.length * math.sin(self.angle))
 
-        self.rect.x = self.x - self.length
-        self.rect.y = self.y - self.length
+        self.rect.x = self.ex
+        self.rect.y = self.ey
         
-        if self.x < 0 or self.y < 0 or self.x > self.game.width or self.y > self.game.height:
+        if self.bx < 0 or self.by < 0 or self.bx > self.game.width or self.by > self.game.height:
             self.kill()
 
     def draw(self, screen):
-        px = int(self.x + 2 * self.length * math.cos(self.angle))
-        py = int(self.y + 2 * self.length * math.sin(self.angle))
-        pygame.draw.line(screen, self.color, (int(self.x), int(self.y)), (int(px), int(py)))
+        pygame.draw.line(screen, self.color, (int(self.bx), int(self.by)), (int(self.ex), int(self.ey)))
