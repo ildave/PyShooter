531808ccfad475688f52272992a49390d9956830
Davide Gualano <ildave@gmail.com>
2018-02-22 17:03:36 +0100

    Rotate bonus

--- PyShooter/bonus.py
+++ PyShooter/bonus.py
@@ -10,23 +10,42 @@ class Bonus(pygame.sprite.Sprite):
         self.size = 10
         self.x = self.enemy.x - self.size / 2
         self.y = self.enemy.y - self.size / 2
-        self.image = pygame.Surface([self.size, self.size])
+        self.image = pygame.Surface([1, 1])
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
         self.vspeed = random.uniform(0.02, 0.06)
         self.hspeed = random.uniform(0.02, 0.06)
         self.angle = enemy.angle
+        self.rotationAngle = random.uniform(0, math.pi / 2)
         angleoffset = random.uniform(0, math.pi / 6) - math.pi / 12
         self.angle += angleoffset
 
+        self.points = ((0, 0), (10, 0), (10, 10), (0, 10))
+        self.points = self.rotate()
+        self.points = [(a + self.x, b + self.y) for a, b in self.points]
+
         self.color = pygame.color.THECOLORS['red']
 
+    def rotate(self):
+        newPoints = []
+        for x, y in self.points:
+            newx = x * math.cos(self.rotationAngle) + y * math.sin(self.rotationAngle)
+            newy = -x * math.sin(self.rotationAngle) + y * math.cos(self.rotationAngle)
+            newPoints.append((newx, newy))
+
+        return newPoints
+
     def update(self, elapsed):
+        self.rotationAngle += -0.01*math.pi
         self.y +=  math.sin(self.angle) * self.vspeed * elapsed 
         self.x += math.cos(self.angle) * self.hspeed * elapsed
-        self.rect.x = self.x
-        self.rect.y = self.y
+        self.rect.x = self.x + self.size / 2
+        self.rect.y = self.y + self.size / 2
+
+        self.points = ((0, 0), (10, 0), (10, 10), (0, 10))
+        self.points = self.rotate()
+        self.points = [(a + self.x, b + self.y) for a, b in self.points]
 
         if not self.inGame():
             self.kill()
@@ -35,4 +54,5 @@ class Bonus(pygame.sprite.Sprite):
         return (self.x > 0 and self.x < self.game.width) and (self.y > 0 and self.y < self.game.height)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect, 1)
+        pygame.draw.polygon(screen, self.color, self.points, 1)
+        #pygame.draw.rect(screen, pygame.color.THECOLORS['white'], self.rect, 1)
