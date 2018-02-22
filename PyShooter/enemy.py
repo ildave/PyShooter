5c77b5bbdb6752d448d3ff6fced6c5368aa2e38e
Davide Gualano <ildave@gmail.com>
2018-02-22 12:48:06 +0100

    Change enemies behaviour

--- PyShooter/enemy.py
+++ PyShooter/enemy.py
@@ -9,34 +9,56 @@ class Enemy(pygame.sprite.Sprite):
         self.radius = random.randint(9, 20)
         self.image = pygame.Surface([self.radius * 2, self.radius * 2])
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, self.game.width - self.radius)
-        self.startX = self.rect.x
-        self.rect.y = 0
+        self.vspeed = random.uniform(0.08, 0.12)
+        self.hspeed = random.uniform(0.08, 0.12)
+        
+        pos = random.randint(0, 3)
+        if pos == 0: #up
+            self.y = -self.radius * 2
+            self.x = random.randint(0, self.game.width - self.radius)
+            self.angle = math.pi / 2
+        if pos == 1: #down
+            self.y = self.game.height + self.radius * 2
+            self.x = random.randint(0, self.game.width - self.radius)
+            self.angle = -math.pi / 2
+        if pos == 2: #left
+            self.x = -self.radius * 2
+            self.y = random.randint(0, self.game.height - self.radius)
+            self.angle = 0
+        if pos == 3: #right
+            self.x = self.game.width + self.radius * 2
+            self.y = random.randint(0, self.game.height - self.radius)
+            self.angle = -math.pi
+
+        angleoffset = random.uniform(0, math.pi / 6) - math.pi / 12
+        self.angle += angleoffset
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
         self.color = pygame.color.THECOLORS['red']
-        self.verticalSpeed = random.uniform(0.095, 0.15)
-        self.amplitude = random.randint(80, 120)
-        if random.randint(1,2) == 1:
-            self.movement = self.sinMovement
-        else:
-            self.movement = self.cosMovement
+        self.active = False
+       
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.rect.x + self.radius, self.rect.y +  self.radius), self.radius)
+        pygame.draw.rect(screen, self.color, self.rect, 1)
 
     def update(self, elapsed, gameScene):
-        self.rect.y += self.verticalSpeed * elapsed
-        if self.rect.y > self.game.height:
+        self.y +=  math.sin(self.angle) * self.vspeed * elapsed 
+        self.x += math.cos(self.angle) * self.hspeed * elapsed
+        self.rect.x = self.x
+        self.rect.y = self.y
+        print(self.x, self.y, self.active, self.inGame())
+        if not self.active and not self.inGame():
+            pass
+        if not self.active and self.inGame():
+            self.active = True
+        if self.active and self.inGame():
+            pass
+        if self.active and not self.inGame():
             self.kill()
-            gameScene.missed += 1
-        self.rect.x = self.movement()
 
-        if self.rect.x < 0:
-            self.rect.x = 0
-        if self.rect.x > self.game.width - self.radius * 2:
-            self.rect.x = self.game.width - self.radius * 2
 
-    def sinMovement(self):
-        return -1 * math.sin(self.rect.y / 50) * self.amplitude + self.startX
-
-    def cosMovement(self):
-        return -1 * math.cos(self.rect.y / 50) * self.amplitude + self.startX
+    def inGame(self):
+        return (self.x > 0 and self.x < self.game.width) and (self.y > 0 and self.y < self.game.height)
