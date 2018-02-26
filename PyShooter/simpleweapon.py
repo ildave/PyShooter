cd08f77196ad89e788748e2469c108139fe1bd1c
Davide Gualano <davide.gualano@siemens.com>
2018-02-26 10:45:06 +0100

    Remove remote files

--- PyShooter/simpleweapon.py
+++
@@ -1,7 +0,0 @@
-import weapon
-import bullet
-
-class SimpleWeapon(weapon.Weapon):
-    def shoot(self):
-        b = bullet.Bullet(self.ship, self.game, 0)
-        self.scene.bullets.add(b)
--- PyShooter/weapon.py
+++
@@ -1,8 +0,0 @@
-class Weapon():
-    def __init__(self, game, ship, scene):
-        self.game = game
-        self.ship = ship
-        self.scene = scene
-
-    def shoot(self):
-        pass
